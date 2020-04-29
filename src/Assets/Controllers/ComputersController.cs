39aba93a9633e05900fd65f2eb9adb3110581fdf
Theunis Swanepoel <theunis.swanepoel@datacom.co.nz>
2020-04-29 22:19:46 +1200

    Implement partial update.

--- src/Assets/Controllers/ComputersController.cs
+++ src/Assets/Controllers/ComputersController.cs
@@ -32,7 +32,8 @@ namespace Assets
         }
 
         [HttpGet]
-        public async Task<ActionResult<IList<Computer>>> SearchAsync(string tenant, ODataQueryOptions<Computer> options)
+        [ProducesResponseType(typeof(IList<Computer>), 200)]
+        public async Task<ActionResult<IList>> SearchAsync(string tenant, ODataQueryOptions<Computer> options)
         { 
             options.Validate(new ODataValidationSettings());
             int? tenantId = await GetTenantIdAsync(tenant);
@@ -48,14 +49,7 @@ namespace Assets
             {
                 scope.Context.Parameters["href"] = _href;
 
-                IQueryable<Computer> query =
-                (
-                    from computer in _db.Computers
-                    where computer.Asset.TenantId == tenantId
-                        && computer.Asset.DeletedDate == null
-                    select computer
-                ).ProjectToType<Computer>(_mapper.Config);
-
+                IQueryable<Computer> query = DbQuery(tenantId.Value).ProjectToType<Computer>(_mapper.Config);
                 models = await options.ApplyTo(query, new ODataQuerySettings { EnsureStableOrdering = false }).ToListAsync();
             }
 
@@ -64,7 +58,7 @@ namespace Assets
 
         [HttpGet("{guid}")]
         [ProducesResponseType(typeof(Computer), 200)]
-        public async Task<ActionResult<Computer>> GetAsync(string tenant, Guid guid, ODataQueryOptions<Computer> options)
+        public async Task<ActionResult<object>> GetAsync(string tenant, Guid guid, ODataQueryOptions<Computer> options)
         {
             options.Validate(new ODataValidationSettings { AllowedQueryOptions = AllowedQueryOptions.Select | AllowedQueryOptions.Expand });
             int? tenantId = await GetTenantIdAsync(tenant);
@@ -80,15 +74,8 @@ namespace Assets
             {
                 scope.Context.Parameters["href"] = _href;
 
-                IQueryable<Computer> query =
-                (
-                    from computer in _db.Computers
-                    where computer.Asset.TenantId == tenantId
-                        && computer.Asset.DeletedDate == null
-                    select computer
-                ).ProjectToType<Computer>(_mapper.Config);
-
-                model = await options.ApplyTo(query.Where(x => x.Guid == guid)).SingleOrDefaultAsync();
+                IQueryable<Computer> query = DbQuery(tenantId.Value, guid).ProjectToType<Computer>(_mapper.Config);
+                model = await options.ApplyTo(query).SingleOrDefaultAsync();
             }
 
             if (model == null)
@@ -109,60 +96,7 @@ namespace Assets
                 return NotFound();
             }
 
-            var entity = new Entities.Computer 
-            {
-                Asset = new Entities.Asset
-                {
-                    TenantId = tenantId.Value,
-                    AssetTypeId = (int)Entities.AssetTypeId.Computer
-                }
-            };
-
-            _mapper.Map(model, entity);
-
-            if (entity.Asset.Guid == default)
-            {
-                entity.Asset.Guid = Guid.NewGuid();
-            }
-
-            entity.Asset.ModifiedDate =
-                entity.Asset.CreatedDate = DateTimeOffset.Now;
-
-            entity.Asset.ModifiedUser = 
-                entity.Asset.CreatedUser = User.Identity.Name;
-
-            _db.Computers.Add(entity);
-            await _db.SaveChangesAsync();
-
-            using (var scope = new MapContextScope())
-            {
-                scope.Context.Parameters["href"] = _href;
-
-                IQueryable<Computer> query =
-                (
-                    from computer in _db.Computers
-                    where computer.Asset.TenantId == tenantId
-                        && computer.Asset.DeletedDate == null
-                    select computer
-                ).ProjectToType<Computer>(_mapper.Config);
-
-                model = await query.SingleOrDefaultAsync(x => x.Guid == entity.Asset.Guid);
-            }
-
-            return Created(model.Href, model);
-        }
-
-        [HttpPut("guid")]
-        public async Task<ActionResult<Computer>> PutAsync(string tenant, Guid guid, Computer model)
-        {
-            int? tenantId = await GetTenantIdAsync(tenant);
-
-            if (tenantId == null)
-            {
-                return NotFound();
-            }
-
-            throw new NotImplementedException();
+            return await PostCoreAsync(tenantId.Value, model);
         }
 
         [HttpPatch("guid")]
@@ -175,7 +109,25 @@ namespace Assets
                 return NotFound();
             }
 
-            throw new NotImplementedException();
+            return await PatchCoreAsync(tenantId.Value, guid, patch);
+        }
+
+        [HttpPut("guid")]
+        public async Task<ActionResult<Computer>> PutAsync(string tenant, Guid guid, Computer model)
+        {
+            if (model.Guid != default && guid != model.Guid)
+            {
+                return BadRequest();
+            }
+
+            int? tenantId = await GetTenantIdAsync(tenant);
+
+            if (tenantId == null)
+            {
+                return NotFound();
+            }
+
+            return await PutCoreAsync(tenantId.Value, guid, model);
         }
 
         private async Task<int?> GetTenantIdAsync(string tenant)
@@ -188,6 +140,106 @@ namespace Assets
                 select new { t.Id };
 
             return (await query.SingleOrDefaultAsync())?.Id;
+        }
+
+        private async Task<ActionResult<Computer>> PostCoreAsync(int tenantId, Computer model)
+        {
+            Guid guid = await CreateCoreAsync(tenantId, model);
+            model = await RetrieveCoreAsync(tenantId, guid);
+
+            return Ok(model);
+        }
+
+        private async Task<ActionResult<Computer>> PatchCoreAsync(int tenantId, Guid guid, JsonPatchDocument<Computer> patch)
+        {
+            Computer model = await RetrieveCoreAsync(tenantId, guid);
+            patch.ApplyTo(model);
+
+            return await PutCoreAsync(tenantId, guid, model);
+        }
+
+        private async Task<ActionResult<Computer>> PutCoreAsync(int tenantId, Guid guid, Computer model)
+        {
+            bool created = await CreateOrUpdateCoreAsync(tenantId, guid, model);
+            model = await RetrieveCoreAsync(tenantId, guid);
+
+            if (created)
+            {
+                return Created(model.Href, model);
+            }
+
+            return Ok(model);
+        }
+
+        private async Task<Computer> RetrieveCoreAsync(int tenantId, Guid guid)
+        {
+            using var scope = new MapContextScope();
+            scope.Context.Parameters["href"] = _href;
+
+            IQueryable<Computer> query = DbQuery(tenantId, guid).ProjectToType<Computer>(_mapper.Config);
+            return await query.SingleOrDefaultAsync();
+        }
+
+        private async Task<bool> CreateOrUpdateCoreAsync(int tenantId, Guid guid, Computer model)
+        {
+            Entities.Computer entity = await DbQuery(tenantId, guid).SingleOrDefaultAsync();
+            
+            if (entity == null)
+            {
+                model.Guid = guid;
+                
+                await CreateCoreAsync(tenantId, model);
+                return true;
+            }
+
+            _mapper.Map(model, entity);
+            Touch(entity, DateTimeOffset.Now, User.Identity.Name);
+            
+            await _db.SaveChangesAsync();
+            return false;
+        }
+
+        private async Task<Guid> CreateCoreAsync(int tenantId, Computer model)
+        {
+            var entity = _mapper.Map<Entities.Computer>(model);
+            _db.Computers.Add(entity);
+
+            Guid guid = model.Guid != default ? model.Guid : Guid.NewGuid();
+            Init(entity, tenantId, guid, DateTimeOffset.Now, User.Identity.Name);
+            
+            await _db.SaveChangesAsync();
+            return guid;
+        }
+
+        private static void Init(Entities.Computer entity, int tenantId, Guid guid, DateTimeOffset now, string user)
+        {
+            entity.Asset ??= new Entities.Asset();
+            entity.Asset.TenantId = tenantId;
+            entity.Asset.Guid = guid;
+            entity.Asset.AssetTypeId = (int)Entities.AssetTypeId.Computer;
+            entity.Asset.CreatedDate = now;
+            entity.Asset.CreatedUser = user;
+            Touch(entity, now, user);
+        }
+
+        private static void Touch(Entities.Computer entity, DateTimeOffset now, string user)
+        {
+            entity.Asset.ModifiedDate = now;
+            entity.Asset.ModifiedUser = user;
+        }
+
+        private IQueryable<Entities.Computer> DbQuery(int tenantId, Guid guid)
+        {
+            return DbQuery(tenantId).Where(x => x.Asset.Guid == guid);
+        }
+
+        private IQueryable<Entities.Computer> DbQuery(int tenantId)
+        {
+            return
+                from computer in _db.Computers.Include(x => x.Asset)
+                where computer.Asset.TenantId == tenantId
+                    && computer.Asset.DeletedDate == null
+                select computer;
         }
     }
 }
